0a46bdde9526945944ead94d4a4cbb9f8bb26d6f
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-28 13:43:30 +0100

    added custom user manager

---
+++ backend/financeAIBackend/core/managers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, username=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required for signup")
+        if username!=None:
+            user = self.model(
+                email = self.normalize_email(email),
+                **extra_fields
+            )
+        else:
+            user = self.model(
+                username=username,
+                email = self.normalize_email(email),
+                **extra_fields
+            )
+
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(
+            username=username,
+            email=email,
+            password=password,
+            **extra_fields
+        )
+    
