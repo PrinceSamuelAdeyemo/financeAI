1c15021e058aa3f6ac20cd31d33fd97cbbee1525
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-28 19:55:49 +0100

    basic viewset for the models for now

--- backend/financeAIBackend/core/views.py
+++ backend/financeAIBackend/core/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, AllowAny
 
+from .models import (
+    User, Category, Notification
+)
+from .serializers import (
+    UserSerializer, CategorySerializer, NotificationSerializer
+)
 # Create your views here.
+class UserViewset(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        return super().get_permissions()
+    
+class CategoryViewset(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+
+class NotificationViewset(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated]
