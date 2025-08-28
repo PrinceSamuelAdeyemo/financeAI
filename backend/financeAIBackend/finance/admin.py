0761f29dbfdd0bf4d5b8685ef30ef14d28dc8096
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-28 20:27:39 +0100

    models addition to the admin

--- backend/financeAIBackend/finance/admin.py
+++ backend/financeAIBackend/finance/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (
+    Account, Transaction, Subscription, Investment, Budget, Goal, AI_Insight
+)
 
 # Register your models here.
+admin.site.register(Account)
+admin.site.register(Transaction)
+admin.site.register(Subscription)
+admin.site.register(Investment)
+admin.site.register(Budget)
+admin.site.register(Goal)
+admin.site.register(AI_Insight)
