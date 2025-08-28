75c0f9f84eccbeb54ce414c46e4f0b4da6a18ed4
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-28 19:56:16 +0100

    added the urls for the current viewset

--- backend/financeAIBackend/core/urls.py
+++ backend/financeAIBackend/core/urls.py
@@ -1,3 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    UserViewset, CategoryViewset, NotificationViewset
+)
+
+
+router = DefaultRouter()
+router.register(r'users', UserViewset)
+router.register(r'categories', CategoryViewset)
+router.register(r'notifications', NotificationViewset)
+
 urlpatterns = [
-    
+    path('', include(router.urls))
 ]
