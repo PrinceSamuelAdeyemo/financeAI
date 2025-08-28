8b7a6a4e8c39c50b88442d6400cf92713d041117
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-28 20:28:26 +0100

    urls for the views

--- backend/financeAIBackend/finance/urls.py
+++ backend/financeAIBackend/finance/urls.py
@@ -1,3 +1,18 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (AccountViewset, TransactionViewset, SubscriptionViewset, InvestmentViewset,
+                    BudgetViewset, GoalViewset, AI_InsightViewset)
+
+router = DefaultRouter()
+
+router.register(r'accounts', AccountViewset)
+router.register(r'transactions', TransactionViewset)
+router.register(r'subscriptions', SubscriptionViewset)
+router.register(r'investments', InvestmentViewset)
+router.register(r'budgets', BudgetViewset)
+router.register(r'goals', GoalViewset)
+router.register(r'ai_insights', AI_InsightViewset)
+
 urlpatterns = [
-    
+    path("", include(router.urls))
 ]
