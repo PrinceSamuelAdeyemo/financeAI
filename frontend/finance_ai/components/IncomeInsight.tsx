73014865f42a95491d7768ee88d8123addbac10a
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-29 23:52:41 +0100

    insightdisplay component added

---
+++ frontend/finance_ai/components/IncomeInsight.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image"
+
+import { InsightDataType } from "@/Types/allTypes"
+
+export default function IncomeInsight(prop : InsightDataType){
+    return (
+        <div className="border-2 border-gray-300 rounded-xl p-4">
+            <div className="flex flex-col">
+                <p>{prop.title}</p>
+                <p className="font-bold text-2xl">{prop.data}</p>
+                <p className="text-sm"><span></span>{prop.remark}</p>
+            </div>
+            <Image src={prop.icon} alt="" />
+        </div>
+    )
+}
