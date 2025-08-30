c133a5daf452fcd5c284ec8d3350c26fcf99af79
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-30 23:20:20 +0100

    minimal padding fix for the page

--- frontend/finance_ai/components/PageContainer.tsx
+++ frontend/finance_ai/components/PageContainer.tsx
@@ -5,7 +5,7 @@ export default function PageContainer({children}: {children: React.ReactNode}){
     return (
         <div className="h-[90vh] relative w-full">
             {/* <DashboardNavbar /> */}
-            <div className="flex flex-col gap-5 h-full overflow-y-scroll sm:px-2 px-4 py-[4vh] lg:h-[90vh]">
+            <div className="flex flex-col gap-5 h-full overflow-y-scroll sm:px-2 px-4 pt-[4vh] pb-[6vh] lg:h-[90vh]">
                 {children}
             </div>
         </div>
