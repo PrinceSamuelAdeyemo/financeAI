baaa07b56a91fc230b67a410a9de119013c5fe9a
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-08-31 14:41:44 +0100

    completed the basic part of the budget page/route

---
+++ frontend/finance_ai/app/dashboard/budgets/page.tsx
@@ -0,0 +1,41 @@
+import dynamic from "next/dynamic";
+import { PiggyBank } from "lucide-react";
+import PageContainer from "@/components/PageContainer";
+
+const Lazy_BudgetInsightCard = dynamic(() => import('@/components/BudgetInsightCard'));
+
+export default function Budgets(){
+
+    return (
+        <PageContainer>
+
+            <div className="flex flex-col lg:flex-row lg:justify-between">
+                <div>
+                    <p className="font-semibold">Budgets</p>
+                    <p className="text-gray-400">Track your spending limits</p>
+                </div>
+                <button className="bg-purple-600 text-white w-full h-10 rounded-xl lg:px-4 py-2 lg:w-fit">
+                    + Create Budget
+                </button>
+            </div>
+
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                <Lazy_BudgetInsightCard />
+                <Lazy_BudgetInsightCard />
+                <Lazy_BudgetInsightCard />
+                <Lazy_BudgetInsightCard />
+                <Lazy_BudgetInsightCard />
+                <Lazy_BudgetInsightCard />
+                <Lazy_BudgetInsightCard />
+
+                <div className="flex flex-col justify-center items-center gap-2 border-2 border-dashed border-gray-300 rounded-xl px-2 py-6 lg:px-4">
+                    <PiggyBank />
+                    <p className="text-sm font-semibold">Create New Budget</p>
+                    <p className="text-sm text-gray-500">Set spending limits for your categories</p>
+                    <button className="border-1 border-gray-400 px-2 py-1 rounded-lg text-sm lg:text-base">+ Add Budget</button>
+                </div>
+            </div>
+             
+        </PageContainer>
+    )
+}
---
+++ frontend/finance_ai/components/BudgetInsightCard.tsx
@@ -0,0 +1,13 @@
+export default function BudgetInsightCard(){
+    return (
+        <div className="flex flex-col gap-2 border-2 border-gray-300 rounded-xl p-2 lg:p-4">
+            <p className="text-sm font-semibold">Groceries</p>
+            <p className="text-sm text-gray-500">#35,200 of #60,000</p>
+            <p>Progress bar</p>
+            <div className="flex justify-between text-xs text-gray-400">
+                <p>80%</p>
+                <p>17% left</p>
+            </div>
+        </div>
+    )
+}
