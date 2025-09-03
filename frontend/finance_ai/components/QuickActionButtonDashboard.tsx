f1acb2bece565fc89d8495ff6465dbf03bc76dca
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-09-03 04:39:16 +0100

    working on category modal

--- frontend/finance_ai/components/QuickActionButtonDashboard.tsx
+++ frontend/finance_ai/components/QuickActionButtonDashboard.tsx
@@ -2,36 +2,34 @@
 
 import Image from "next/image";
 import { useEffect, useState } from "react";
+
 import { ModalHandlerType } from "@/Types/allTypes";
 
 import BaseModal from "./modals/BaseModal";
+import CreateCategoryModal from "./modals/CreateCategoryModal";
 import Transaction_Receipt_Buttons from "./Transaction_Receipt_Buttons";
 
 
 export default function QuickActionButtonDashboard(){
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+    const [isCategoryModalOpen, setIsCategoryModalOpen] = useState<boolean>(false);
+    const [isBudgetModalOpen, setIsBudgetModalOpen] = useState<boolean>(false);
+    const [isSetgoalModalOpen, setIsSetgoalModalOpen] = useState<boolean>(false);
 
-    const openCreateCategoryModal = (isModalOpen: boolean) => {
-        setIsModalOpen(true);
-    }
-
-    const openCreateBudgetModal = () => {
-        alert("Hello")
-    }
-
+    
     return (
         <div className="flex flex-col gap-2">
-            <Transaction_Receipt_Buttons openModalFunc={setIsModalOpen} />
+            <Transaction_Receipt_Buttons openCreateCategoryModal={setIsCategoryModalOpen} />
             <div className="flex gap-2 w-full">
-                <button onClick={() => setIsModalOpen(true)} className="flex flex-col justify-center items-center border-1 border-gray-200 rounded-xl h-[10vh] w-1/2">
+                <button className="flex flex-col justify-center items-center border-1 border-gray-200 rounded-xl h-[10vh] w-1/2">
                     <Image src="" alt="" />
                     <p>Create Budget</p>
                 </button>
-                <button onClick={() => setIsModalOpen(true)} className="flex flex-col justify-center items-center border-1 border-gray-200 rounded-xl h-[10vh] w-1/2">
+                <button className="flex flex-col justify-center items-center border-1 border-gray-200 rounded-xl h-[10vh] w-1/2">
                     <p>Set Goal</p>
                 </button>
             </div>
-            {isModalOpen && <BaseModal isOpenModal={setIsModalOpen} />}
+            {/* {isModalOpen && <BaseModal modalOpen={setIsModalOpen} />} */}
+            {isCategoryModalOpen && <CreateCategoryModal categoryModalOpen={setIsCategoryModalOpen} />}
         </div>
     )
 }
