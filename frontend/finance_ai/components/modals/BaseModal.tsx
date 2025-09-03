79a8ea8a371f74b55f54c6fe5bfc154b83d2713e
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-09-03 04:40:29 +0100

    minor changes in the base modal

--- frontend/finance_ai/components/modals/BaseModal.tsx
+++ frontend/finance_ai/components/modals/BaseModal.tsx
@@ -3,16 +3,16 @@ import { X } from "lucide-react"
 
 import { ModalHandlerType } from "@/Types/allTypes"
 
-export default function BaseModal({children, isOpenModal}: {children?: React.ReactNode} & {isOpenModal?: ModalHandlerType}){
+export default function BaseModal({children, modalOpen}: {children?: React.ReactNode} & {modalOpen?: ModalHandlerType}){
     return (
         /* Modal background layer */
         <div className="flex justify-center items-center w-full h-[93vh] top-0 left-0 absolute z-10">
             {/* Main modal parent */}
             <div className="relative bg-white border-1 border-gray-400 shadow-xl h-[80%] w-[90%] lg:w-[80%]">
-                <button onClick={() => isOpenModal?.(false)} className="absolute top-2 right-2">
+                <button onClick={() => modalOpen?.(false)} className="absolute top-2 right-2">
                     <X className="text-red-500" />
                 </button>
-                <div className="absolute bottom-0 w-full h-[95%] overflow-y-auto">
+                <div className="flex justify-center absolute bottom-0 w-full h-[95%] overflow-y-auto">
                     {children}
                 </div>
             </div>
