92ef30b3718e612c324f462ce8fbdaf2cb3ef287
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-09-03 04:39:34 +0100

    category modal completed

--- frontend/finance_ai/components/Transaction_Receipt_Buttons.tsx
+++ frontend/finance_ai/components/Transaction_Receipt_Buttons.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image"
+
+import CreateCategoryModal from "./modals/CreateCategoryModal"
 import { ModalHandlerType } from "@/Types/allTypes"
 
-export default function Transaction_Receipt_Buttons({openModalFunc}: {openModalFunc: ModalHandlerType}){
+export default function Transaction_Receipt_Buttons({openCreateCategoryModal}: {openCreateCategoryModal?: ModalHandlerType}){
+    
     return (
         <div className="flex gap-2 w-full">
-            <div className="flex flex-col justify-center items-center border-1 border-gray-200 rounded-xl h-[10vh] w-1/2 bg-purple-900">
+            <button className="flex flex-col justify-center items-center border-1 border-gray-200 rounded-xl h-[10vh] w-1/2 bg-purple-900">
                 <p className="text-white">+</p>
                 <p className="text-white">Add Transaction</p>
-            </div>
-            <button onClick={() => openModalFunc(true)} className="flex flex-col justify-center items-center border-1 border-gray-200 rounded-xl h-[10vh] w-1/2">
+            </button>
+            <button onClick={() => openCreateCategoryModal?.(true)} className="flex flex-col justify-center items-center border-1 border-gray-200 rounded-xl h-[10vh] w-1/2">
                 <Image src="" alt="" />
                 <p>Create Category</p>
             </button>
