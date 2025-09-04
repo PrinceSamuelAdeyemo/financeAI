7a79bf0d8829273f7cfaf29a70fd3bbcaa2bcd7b
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-09-04 22:01:45 +0100

    added progressbar to budget insight card

--- frontend/finance_ai/components/BudgetInsightCard.tsx
+++ frontend/finance_ai/components/BudgetInsightCard.tsx
@@ -1,12 +1,15 @@
+import Progressbar from "./ui/Progressbar"
+
 export default function BudgetInsightCard(){
     return (
         <div className="flex flex-col gap-2 border-2 border-gray-300 rounded-xl p-2 lg:p-4">
             <p className="text-sm font-semibold">Groceries</p>
             <p className="text-sm text-gray-500">#35,200 of #60,000</p>
             <p>Progress bar</p>
+            <Progressbar position={80} color="purple-500" />
             <div className="flex justify-between text-xs text-gray-400">
                 <p>80%</p>
-                <p>17% left</p>
+                <p>20% left</p>
             </div>
         </div>
     )
