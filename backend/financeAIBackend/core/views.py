79f10fbfacf8ae5cf5de56b24e695d5543576628
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-09-06 23:57:31 +0100

    working around deriving top user categories spendings

--- backend/financeAIBackend/core/views.py
+++ backend/financeAIBackend/core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.db import models
+from django.db.models import Sum
 from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, AllowAny
 
 from .models import (
@@ -23,6 +26,13 @@ class CategoryViewset(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated]
+
+    @action(methods='GET', detail=False, url_path='/user-top-categories')
+    def users_top_five_categories_spending(self):
+        top_categories = Category.objects.annotate(
+            total_spent = Sum('transactions__amount'), filter=models.Q(transactions__user=request.user)
+            ).filter(total_spent__isnull=False
+                     ).order_by('-total_spent')[:5]
 
 
 class NotificationViewset(viewsets.ModelViewSet):
