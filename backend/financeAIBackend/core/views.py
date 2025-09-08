e82f0df71c2bd93eae8c88fe3192cb27b112a74f
PrinceSamuelAdeyemo <crownprincesamuelaadeyemo@gmail.com>
2025-09-08 11:39:59 +0100

    added view for getting user top five categories

--- backend/financeAIBackend/core/views.py
+++ backend/financeAIBackend/core/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.db import models
-from django.db.models import Sum
+from django.db.models import Sum, Q
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, AllowAny
 
 from .models import (
@@ -27,12 +27,18 @@ class CategoryViewset(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated]
 
-    @action(methods='GET', detail=False, url_path='/user-top-categories')
-    def users_top_five_categories_spending(self):
+    @action(methods=['GET'], detail=False, url_path='user-top-categories')
+    def users_top_five_categories_spending(self, request):
+        top_categories_array = []
         top_categories = Category.objects.annotate(
-            total_spent = Sum('transactions__amount'), filter=models.Q(transactions__user=request.user)
+            total_spent = Sum('transactions__amount'), filter=Q(transactions__user=request.user)
             ).filter(total_spent__isnull=False
                      ).order_by('-total_spent')[:5]
+        
+        for category in top_categories:
+            top_categories_array.append({category.name: category.total_spent})
+            
+        return Response({"message": top_categories_array})
 
 
 class NotificationViewset(viewsets.ModelViewSet):
